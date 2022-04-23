2fd1ea5eb06e0cee2a1c77944194c2623740ca16
dadiego <daniel.diego@outlook.com>
2022-04-23 12:33:41 -0500

    Mocha testing small demo

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -1,7 +1,5 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const { beforeEach } = require('mocha');
-const { describe } = require('mocha');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
